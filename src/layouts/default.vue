cd37fb50170aab0f508c1fd7752c3fb3818eda49
rainboy <rainboylvx@qq.com>
2023-04-23 15:13:01 +0800

    init

---
+++ src/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <main
+    px-4 py-10
+    text="center gray-700 dark:gray-200"
+  >
+    <div flex>
+        <treeMenuWrapper :menuArray="bookMenu.child">
+        </treeMenuWrapper>
+        <RouterView />
+    </div>
+    <TheFooter />
+    <div mx-auto mt-5 text-center text-sm opacity-50>
+      [Default Layout]
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import treeMenuWrapper from '../components/treeMenu/treeMenuWrapper.vue'
+import {bookMenu} from '../menu'
+
+</script>
---
+++ src/menu.ts
@@ -0,0 +1,52 @@
+//书的目录
+
+export interface MenuInter {
+
+    title:string
+    src:string
+    child?: MenuInter[]
+}
+
+
+export const bookMenu : MenuInter = {
+    title: 'root',
+    src:'',
+    child : [
+        {
+            title:'cpp11',
+            src:'cpp11',
+            child: [
+                {
+                    title:'regex',
+                    src:'regex/1.md'
+                },
+                {
+                    title:'refercence_wrappers',
+                    src:'refercence_wrappers/index.md',
+                }
+            ]
+        },
+        {
+            title:'cpp20',
+            src:'cpp20',
+            child: [
+                {
+                    title:"range",
+                    src:'ranges/index.md'
+                }
+            ]
+
+        },
+        {
+            title:'奇技淫巧',
+            src:'奇技淫巧',
+            child : [
+                {
+                    title:'读写锁',
+                    src:'读写锁/readme.md',
+                }
+            ]
+
+        }
+    ]
+}
